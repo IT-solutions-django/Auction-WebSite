844d4213f1b80bbdc02867872265e76e96fbf3ac
finch <uroddaunich@gmail.com>
2024-09-18 08:59:21 +1000

    Убрал лишний на данный момент сериализатор и ненужные импорты

--- cars/api/serializres.py
+++ cars/api/serializres.py
@@ -1,28 +1,31 @@
 from rest_framework import serializers
 from cars.models import Car
-from jp_center.models import FrontCar
-
-
 
 
 
 class CarSerializer(serializers.ModelSerializer):
-    make = serializers.SerializerMethodField()
+    """Используем такое большое количество методов,
+    чтобы заменить 'некрасивые' значения полей 
+    на более симпатичные"""
+
+    make = serializers.SerializerMethodField() 
     model = serializers.SerializerMethodField()
+
     color = serializers.SerializerMethodField()
     kpp = serializers.SerializerMethodField()
+
     priv = serializers.SerializerMethodField()
-    
+
     class Meta:
         model = Car
-        fields = '__all__'
-    
+        fields = "__all__"
+
     def get_kpp(self, obj):
         return obj.kpp.true_kpp if obj.kpp else None
-    
+
     def get_priv(self, obj):
         return obj.priv.true_priv if obj.priv else None
-    
+
     def get_make(self, obj):
         return obj.make.name if obj.make else None
 
@@ -32,41 +35,8 @@ class CarSerializer(serializers.ModelSerializer):
     def get_color(self, obj):
         return obj.color.true_color if obj.color else None
 
-class MainPageSerializer(serializers.ModelSerializer):
-    make = serializers.SerializerMethodField()
-    model = serializers.SerializerMethodField()
-    color = serializers.SerializerMethodField()
-    kpp = serializers.SerializerMethodField()
-    privod = serializers.SerializerMethodField()
-    export_country = serializers.SerializerMethodField()
-    body_type = serializers.SerializerMethodField()
-    wheel_position = serializers.SerializerMethodField()
+"""Вообще основная идея в том, 
+чтобы избежать создания новых классов для новой модели
+вместо этого я решил, что обе модели будут использовать 
+одни и те же классы, но по разному"""
 
-
-    class Meta:
-        model = FrontCar
-        fields = '__all__'
-    
-    def get_kpp(self, obj):
-        return obj.kpp.true_kpp if obj.kpp else None
-    
-    def get_privod(self, obj):
-        return obj.privod.true_priv if obj.privod else None
-    
-    def get_export_country(self, obj):
-        return obj.export_country.name if obj.export_country else None
-    
-    def get_body_type(self, obj):
-        return obj.body_type.name if obj.body_type else None
-    
-    def get_wheel_position(self, obj):
-        return obj.wheel_position.name if obj.wheel_position else None
-    
-    def get_make(self, obj):
-        return obj.make.name if obj.make else None
-
-    def get_model(self, obj):
-        return obj.model.name if obj.model else None
-
-    def get_color(self, obj):
-        return obj.color.true_color if obj.color else None
